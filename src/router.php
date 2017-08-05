ad8d6ed1f85ace32c64fc9142777b243817c53d9
Kohki Makimoto <kohki.makimoto@gmail.com>
2017-08-05 18:49:04 +0900

    update

--- src/router.php
+++ src/router.php
@@ -6,6 +6,7 @@ $theme = getenv("ADMINER_THEME_PATH");
 if (preg_match('/\/adminer\.css/', $_SERVER["REQUEST_URI"]) && $theme) {
     if (file_exists($theme)) {
         header("Content-Type: text/css");
+        header('Cache-Control: max-age=0');
         echo file_get_contents($theme);
         return false;
     }
